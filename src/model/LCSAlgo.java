d70c0c510965877b0c4fcf88ac9496585c171f28
Nerrtica <nerrtica@naver.com>
2016-05-14 17:02:28 +0900

    LCS 알고리즘 관련 class 생성 LCS의 길이를 연산하는 LCSLength method와 하나의 LCS를 읽어오는 readLCS method 구현

---
+++ src/model/LCSAlgo.java
@@ -0,0 +1,40 @@
+package model;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class LCSAlgo {
+    public static int[][] LCSLength(List<String> A, List<String> B) {
+        int ASize = A.size(), BSize = B.size();
+        int[][] C = new int[ASize + 1][BSize + 1];
+
+        for (int i = 0; i <= ASize; i++) { C[i][0] = 0; }
+        for (int j = 0; j <= BSize; j++) { C[0][j] = 0; }
+        for (int i = 1; i <= ASize; i++) {
+            for (int j = 1; j <= BSize; j++) {
+                if (A.get(i - 1).equals(B.get(j - 1))) {
+                    C[i][j] = C[i - 1][j - 1] + 1;
+                } else {
+                    C[i][j] = Math.max(C[i][j - 1], C[i - 1][j]);
+                }
+            }
+        }
+
+        return C;
+    }
+
+    public static List<String> readLCS(int[][] C, List<String> A, List<String> B, int i, int j) {
+        if (i == 0 || j == 0) { return new ArrayList<String>(); }
+        else if (A.get(i - 1).equals(B.get(j - 1))) {
+            List<String> newList = readLCS(C, A, B, i - 1, j - 1);
+            newList.add(A.get(i - 1));
+            return newList;
+        } else {
+            if (C[i][j - 1] > C[i - 1][j]) {
+                return readLCS(C, A, B, i, j - 1);
+            } else {
+                return readLCS(C, A, B, i - 1, j);
+            }
+        }
+    }
+}
