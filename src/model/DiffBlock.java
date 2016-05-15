6c6bde937ee1970f2d925ff169e3960e22593eef
Nerrtica <nerrtica@naver.com>
2016-05-15 15:31:01 +0900

    add DiffBlock class

---
+++ src/model/DiffBlock.java
@@ -0,0 +1,50 @@
+package model;
+
+/**
+ * Created by Nerrtica on 2016. 5. 15..
+ */
+public class DiffBlock {
+    private int startIndex;
+    private int endIndex;
+    private int lineNumber;
+
+    public DiffBlock (int startIndex, int endIndex) {
+        if (startIndex > endIndex) {
+            //에러 발생
+            return;
+        }
+        this.startIndex = startIndex;
+        this.endIndex = endIndex;
+        this.lineNumber = endIndex - startIndex + 1;
+    }
+
+    public void changeStartIndex (int startIndex) {
+        if (startIndex > this.endIndex) {
+            //에러 발생
+            return;
+        }
+        this.startIndex = startIndex;
+        this.lineNumber = this.endIndex - startIndex + 1;
+    }
+
+    public void changeEndIndex (int endIndex) {
+        if (this.startIndex > endIndex) {
+            //에러 발생
+            return;
+        }
+        this.endIndex = endIndex;
+        this.lineNumber = endIndex - this.startIndex + 1;
+    }
+
+    public int getStartIndex () {
+        return startIndex;
+    }
+
+    public int getEndIndex () {
+        return endIndex;
+    }
+
+    public int getLineNumber () {
+        return lineNumber;
+    }
+}
