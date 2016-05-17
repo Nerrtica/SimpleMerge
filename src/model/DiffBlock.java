477e091ee90fe7f478e9ba068c6abd9b07a8d539
Nerrtica <nerrtica@naver.com>
2016-05-17 15:33:58 +0900

    startIndex가 endIndex보다 클 경우의 예외처리 제거

--- src/model/DiffBlock.java
+++ src/model/DiffBlock.java
@@ -7,10 +7,6 @@ public class DiffBlock {
 
     /* Constructor */
     public DiffBlock (int startIndex, int endIndex) {
-        if (startIndex > endIndex) {
-            //에러 발생
-            return;
-        }
         this.startIndex = startIndex;
         this.endIndex = endIndex;
         this.lineNumber = endIndex - startIndex + 1;
@@ -18,20 +14,12 @@ public class DiffBlock {
 
     /* 시작 Index 변경 method */
     public void changeStartIndex (int startIndex) {
-        if (startIndex > this.endIndex) {
-            //에러 발생
-            return;
-        }
         this.startIndex = startIndex;
         this.lineNumber = this.endIndex - startIndex + 1;
     }
 
     /* 끝 Index 변경 method */
     public void changeEndIndex (int endIndex) {
-        if (this.startIndex > endIndex) {
-            //에러 발생
-            return;
-        }
         this.endIndex = endIndex;
         this.lineNumber = endIndex - this.startIndex + 1;
     }
