21bdd2b50385319d540db0d551a50a7047fc7fc9
Nerrtica <nerrtica@naver.com>
2016-05-18 22:01:01 +0900

    Controller와 View 1차 연결

--- src/controller/ClsController.java
+++ src/controller/ClsController.java
@@ -2,15 +2,17 @@ package controller;
 
 import view.ClsView;
 import model.ImportedFile;
+import model.DiffBlock;
 import java.awt.EventQueue;
 import java.util.ArrayList;
+import java.util.List;
 
 public class ClsController {
     ImportedFile leftFile = new ImportedFile();
     ImportedFile rightFile = new ImportedFile();
     ClsView window;
 
-    public ClsController()
+    public ClsController ()
     {
         EventQueue.invokeLater(new Runnable()
         {
@@ -22,7 +24,7 @@ public class ClsController {
 
     }
 
-    private void show() {
+    private void show () {
         try
         {
             window = new ClsView(this);
@@ -32,7 +34,7 @@ public class ClsController {
         }
     }
 
-    public ArrayList<String> load(String fileRoute, boolean isLeftFile) {
+    public ArrayList<String> load (String fileRoute, boolean isLeftFile) {
         if (isLeftFile) {
             leftFile.load(fileRoute);
             return leftFile.getText();
@@ -42,6 +44,44 @@ public class ClsController {
         }
     }
 
+    public void save (boolean isLeftFile) {
+        if (isLeftFile) {
+            leftFile.save();
+        } else {
+            rightFile.save();
+        }
+    }
 
+    public void saveAs (String fileRoute, boolean isLeftFile) {
+        if (isLeftFile) {
+            leftFile.saveAs(fileRoute);
+        } else {
+            rightFile.saveAs(fileRoute);
+        }
+    }
+
+    public void edit (String document, ArrayList<Boolean> bool, boolean isLeftFile) {
+        if (isLeftFile) {
+            leftFile.convert(document, bool);
+        } else {
+            rightFile.convert(document, bool);
+        }
+    }
+
+    public List<DiffBlock> compare (boolean isLeftFile) {
+        if (isLeftFile) {
+            return leftFile.compare(rightFile);
+        } else {
+            return rightFile.compare(leftFile);
+        }
+    }
+
+    public void merge (List<DiffBlock> originalBlockList, List<DiffBlock> targetBlockList, int blockIndex, boolean isLeftfile) {
+        if (isLeftfile) {
+            leftFile.merge(rightFile, originalBlockList, targetBlockList, blockIndex);
+        } else {
+            rightFile.merge(leftFile, originalBlockList, targetBlockList, blockIndex);
+        }
+    }
 
 }
