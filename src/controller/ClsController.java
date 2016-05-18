78e503b12d4ea35c2ba7aead43b482e7d3a253c8
Nerrtica <nerrtica@naver.com>
2016-05-18 20:43:35 +0900

    show method 생성하여 ClsView 인스턴스 만드는 부분 옮김

--- src/controller/ClsController.java
+++ src/controller/ClsController.java
@@ -5,24 +5,26 @@ import java.awt.EventQueue;
 
 public class ClsController {
 
-
     public ClsController()
     {
         EventQueue.invokeLater(new Runnable()
         {
             public void run()
             {
-                try
-                {
-                    ClsView window = new ClsView(this);
-                } catch (Exception e)
-                {
-                    e.printStackTrace();
-                }
+               show();
             }
         });
 
+    }
 
+    private void show() {
+        try
+        {
+            ClsView window = new ClsView(this);
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 
 }
